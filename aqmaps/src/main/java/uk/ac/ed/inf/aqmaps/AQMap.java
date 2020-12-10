0fd84723bb7bee0a27a8f6e1cfcbcd9e63419361
micksadowsky <micsad999@gmail.com>
2020-12-10 13:45:46 +0100

    Cleaned up imports

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/AQMap.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/AQMap.java
@@ -3,7 +3,6 @@ package uk.ac.ed.inf.aqmaps;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import com.mapbox.geojson.Feature;
